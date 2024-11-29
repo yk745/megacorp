f5b224c351dea1b553d7342950f36405042c3cd0
Yong Khee <80207506+yk745@users.noreply.github.com>
2024-11-29 18:47:39 +0800

    P: Revert "K: credit card scanner"
    This reverts commit dd126870ff91745c2c0ce35e4ecd4b0ff4cdb50e.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,8 +1,1 @@
-echo "====== SCANINNG FOR CREDIT CARD NUMBERS ======"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "====== CREDIT CARD SCAN COMPLETE ======"
-echo "====== SCANINNG FOR SOCIAL SECURITY NUMBERS ======"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "====== SOCIAL SECURITY SCAN COMPLETE ======"
-echo "====== SCANINNG FOR PHONE NUMBERS ======"
-echo "====== PHONE SCAN COMPLETE ======"
+# TODO: write the script
